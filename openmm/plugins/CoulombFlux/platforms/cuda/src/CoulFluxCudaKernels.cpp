7ee9e188dbcc882a6b1491dd09e2503dc89a9c67
WangXinyan940 <44082181+WangXinyan940@users.noreply.github.com>
2020-11-03 23:05:15 +0800

    Update CoulFluxCudaKernels.cpp
    Fullfill some code to initialize data structures.

--- openmm/plugins/CoulombFlux/platforms/cuda/src/CoulFluxCudaKernels.cpp
+++ openmm/plugins/CoulombFlux/platforms/cuda/src/CoulFluxCudaKernels.cpp
@@ -40,6 +40,8 @@
 
 #include <algorithm>
 #include <cmath>
+#include <vector>
+#include <set>
 #ifdef _MSC_VER
 #include <windows.h>
 #endif
@@ -65,30 +67,90 @@ CudaCalcCoulFluxKernel::~CudaCalcAmoebaMultipoleForceKernel() {
 void CudaCalcCoulFluxKernel::initialize(const System& system, const CoulFluxForce& force) {
     cu.setAsCurrent();
     int elementSize = (cu.getUseDoublePrecision() ? sizeof(double) : sizeof(float));
-
-    bool useEwald = xxx; // wait for finish
+    bool useEwald = (force.NonbondedMethod == 1);
+    numParticles = force.getNumParticles();
 
     // - Initialize parameters.
     //    - CudaArray dQdXidx;
+    vector<set<int>> dQdXidxSet;
+    for(int i = 0; i < numParticles; i++){
+        set<int> tmp;
+        dQdXidxSet.push_back(tmp);
+    }
+    for(int i = 0; i < force.getNumCoulFluxBond(); i++){
+        int a, b; 
+        double r0, j;
+        force.getCoulFluxBond(i, a, b, r0, j);
+        dQdXidxSet[a].insert(a);
+        dQdXidxSet[a].insert(b);
+        dQdXidxSet[b].insert(a);
+        dQdXidxSet[b].insert(b);
+    }
+
+    for(int i = 0; i < force.getNumCoulFluxAngle(); i++){
+        int a, b, c;
+        double theta0, ja, jc;
+        force.getCoulFluxAngle(i, a, b, c, theta0, ja, jc);
+        dQdXidxSet[a].insert(a);
+        dQdXidxSet[a].insert(b);
+        dQdXidxSet[a].insert(c);
+        dQdXidxSet[b].insert(a);
+        dQdXidxSet[b].insert(b);
+        dQdXidxSet[b].insert(c);
+        dQdXidxSet[c].insert(a);
+        dQdXidxSet[c].insert(b);
+        dQdXidxSet[c].insert(c);
+    }
+    int maxnumdX = 0;
+    for(int i=0; i < numParticles; i++){
+        if (dQdXidxSet[i].size() > maxnumdX){
+            maxnumdX = dQdXidxSet[i].size();
+        }
+    }
+
+    vector<int> dQdXidxVec;
+    for(int i = 0; i < numParticles*maxnumdX; i++){
+        dQdXidxVec.push_back(0);
+    }
+    for(int i = 0; i < numParticles; i++){
+        int numdX = 0;
+        for(set<int>::iterator it=dQdXidxSet.begin() ;it!=dQdXidxSet.end();it++){
+            dQdXidxVec[i * maxnumdX + numdX] = *it;
+            numdX++;
+        }
+    }
     //    - CudaArray dQdXval;
+    vector<float> dQdXvalVec;
+    for(int i = 0; i < numParticles*maxnumdX; i++){
+        dQdXvalVec.push_back(0.0);
+    }
     //    - CudaArray dEdQ;
-    //    - CudaArray CosSin; 
+    vector<float> dEdQVec;
+    for(int i = 0; i < numParticles; i++){
+        dEdQVec.push_back(0.0);
+    }
     //    - CudaArray initCharge;
+    vector<float> initChargeVec;
+    for(int i = 0; i < numParticles; i++){
+        initChargeVec.push_back(0.0);
+    }
     //    - CudaArray realCharge;
-    numParticles = force.getNumParticles();
-    dQdXidx.initialize() // wait for finish
-    dQdXval.initialize() // wait for finish
+    vector<float> realChargeVec;
+    for(int i = 0; i < numParticles; i++){
+        realChargeVec.push_back(0.0);
+    }
+    
+    dQdXidx.initialize<int>(cu, numParticles*maxnumdX, "dQdXidx"); // wait for finish
+    dQdXval.initialize<float>(cu, numParticles*maxnumdX, "dQdXval"); // wait for finish
     dEdQ.initialize(cu, numParticles, elementSize, "dEdQ");
     initCharge.initialize(cu, numParticles, elementSize, "initCharge");
     realCharge.initialize(cu, numParticles, elementSize, "realCharge");
 
     // if use Ewald
-    if (...){
+    if (useEwald){
         // calc kmaxx, kmaxy, kmaxz
         CosSin.initialize(cu, 2*(2*kmaxx-1)*(2*kmaxy-1)*(2*kmaxz-1), elementSize, "CosSin");
     }
-
-    // -- Update initCharge
 
     // -- Update exceptions
     numExceptions = force.getNumExceptions();
@@ -111,7 +173,7 @@ void CudaCalcCoulFluxKernel::initialize(const System& system, const CoulFluxForc
     }
 
     // -- initialize Neighborlist
-    cu.getNonbondedUtilities().addInteraction(usePME, usePME, true, force.getCutoffDistance(), exclusions, "", force.getForceGroup());
+    cu.getNonbondedUtilities().addInteraction(true, true, true, force.getCutoffDistance(), exclusions, "", force.getForceGroup());
     cu.getNonbondedUtilities().setUsePadding(false);
 
     // -- Build some kernels: calc constants
